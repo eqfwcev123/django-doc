3907d1cc9e6fea0d00bec315c761d8d8ea8eb68f
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2019-12-18 17:35:55 +0900

    initial settings for myapp

---
+++ app/myapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+class Person(models.Model):
+    SHIRT_SIZE = (
+        ('S', 'SMALL'),
+        ('M', 'MEDIUM'),
+        ('L', 'LARGE'),
+    )
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
